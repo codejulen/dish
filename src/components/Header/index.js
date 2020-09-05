3755d46265bd67ce70385710115ee4f2b8d0f048
Julian <julianzabala1@gmail.com>
2020-09-05 01:26:51 -0700

    Initial stupid commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { HeaderStyle } from "./index.style";
+
+export const Header = () => {
+  return (
+    <HeaderStyle>
+      <h1>Hello World</h1>
+    </HeaderStyle>
+  );
+};
+
+export default Header;
